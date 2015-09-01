f38a12c42b8dce6b912a3f4d3102d0195c9fb575
andrew dang & clayton easterbrooks <student@epicodus.com>
2015-09-01 15:30:36 -0700

    add game spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,3 +7,11 @@ describe('#initialize') do
   end
 
 end
+
+describe('#turn') do
+  it('creates new game object, initializes first values') do
+    test_game = Game.create({:happiness => 0, :stop_id => 1, :random_id => 0, :energy => 0, :money => 80, :name => "Jan"})
+    expect(test_game.happiness).to(eq(0))
+  end
+
+end
