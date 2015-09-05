3ac8e77394d121773cc9b10cc9bcc2b01b6b9af8
Christian <christian.lei1@gmail.com>
2015-09-04 20:07:13 -0700

    .

--- db/migrate/20150902165853_add_cafe_events_to_events.rb
+++
@@ -1,7 +0,0 @@
-# class AddCoffeeShopEventsToEvents < ActiveRecord::Migration
-#   def change
-#     remove_column(:random_events, :description, :string)
-#     add_column(:random_events, :bar, :string)
-#     add_column(:random_events, :coffee_shop, :string)
-#   end
-# end
